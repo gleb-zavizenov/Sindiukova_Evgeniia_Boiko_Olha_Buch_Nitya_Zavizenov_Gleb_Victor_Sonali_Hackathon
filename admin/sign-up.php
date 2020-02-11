967d4ec18fa4159fc46b3774cd5dd58fede60980
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2020-02-11 00:19:36 -0500

    Chenged file location

--- admin/sign-up.php
+++ admin/sign-up.php
@@ -43,7 +43,7 @@ function update_db($first_name,$last_name, $email, $country) {
 
 
     // add redirect back to index.html
-    header('Refresh:5; url=../index.html');
+    header('Refresh:5; url=index.html');
     exit;
        
 
@@ -73,7 +73,7 @@ function update_db($first_name,$last_name, $email, $country) {
         };
 
         // add redirect back to index.html
-        header('Refresh:5; url=../index.html');
+        header('Refresh:5; url=index.html');
         exit;
     } 
 }
